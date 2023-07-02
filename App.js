48e6e3a066442942b345d83406f33788cc0b93fc
jellyfish-17 <vikramn06081999@gmail.com>
2023-07-02 11:44:21 +0530

    updated git ignore

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Welcome</Text>
+      <Text>Welcome !</Text>
       <StatusBar style="auto" />
     </View>
   );
